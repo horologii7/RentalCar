26031067388cf67d1bb7758ddc9cda715089c4fd
Ruan Felipe <ruanfelipedpontes@gmail.com>
2019-12-27 11:57:36 -0300

    Adicionando o começo do programa

--- RentalCar/Program.cs
+++ RentalCar/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using Entities;
 
 namespace RentalCar
 {
@@ -6,7 +8,14 @@ namespace RentalCar
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (dd/mm/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/mm/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
         }
     }
 }
