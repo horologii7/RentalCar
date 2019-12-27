4bd6c3a1300cc2d2cf2c01db00db317ef8086215
Ruan Felipe <ruanfelipedpontes@gmail.com>
2019-12-27 12:52:51 -0300

    Service BrazilTaxService adicionado

---
+++ RentalCar/Services/BrazilTaxService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    class BrazilTaxService
+    {
+        public double tax(double amount)
+        {
+            if (amount <= 100) return amount * 0.2;
+            else return amount * 0.15;
+        }
+    }
+}
