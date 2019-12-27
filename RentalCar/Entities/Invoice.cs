434782351ecd66d1c2263c13ea5673ba24b91e02
Ruan Felipe <ruanfelipedpontes@gmail.com>
2019-12-27 11:42:52 -0300

    Entidade Invoice adicionada

---
+++ RentalCar/Entities/Invoice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    class Invoice
+    {
+        public double basicPayment { get; set; }
+        public double tax { get; set; }
+        public double totalPayment { get { return basicPayment + tax; } }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            this.basicPayment = basicPayment;
+            this.tax = tax;
+        }
+
+    }
+}
